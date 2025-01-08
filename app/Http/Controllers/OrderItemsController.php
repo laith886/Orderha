79da07dc8f1adc0f42447d27805b571dbc3d95cf
Laith <laith.yns@gmail.com>
2025-01-09 01:07:44 +0300

    Initial commit

---
+++ app/Http/Controllers/OrderItemsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CartItems;
+use App\Models\OrderItems;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class OrderItemsController extends Controller
+{
+
+
+
+
+
+}
